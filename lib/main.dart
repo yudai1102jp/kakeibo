3c1bcfd3218eb227779e70c0a7bc2c67a18d4548
yudai1102jp <yudaiikenoya@gmail.com>
2021-11-18 18:16:17 +0900

    firebase導入 パッケージインストール

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:kakeibo/ui/login/login.dart';
+
 import './ui/input/input.dart';
 import './ui/history/history.dart';
+
 import './ui/history/test.dart';
 import './ui/history/test2.dart';
+import './ui/login/login.dart';
 import './widget/selectlist.dart';
+
 // import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
 // import 'package:flutter/services.dart';
@@ -20,11 +29,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext constext) {
     return MaterialApp(
       // title: 'Startup Name Generator',
-      initialRoute: '/',
+      initialRoute: '/login',
       routes: {
         '/': (context) => InputPage(),
         '/h': (context) => History(),
         '/test': (context) => DropDownUserList(),
+        '/login': (context) => Login(),
       },
     );
   }
